eeda3d2e1c8380c101bf12ae8596cb5b82b73b72
david wu <davidwuguantong@gmail.com>
2025-10-04 04:18:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "fintrack-mini-pwa-296545-f7949",
+  "appId": "1:94520355691:web:e08ecdfa0a9ca166ecba2e",
+  "apiKey": "AIzaSyB-Wzi-vA5j8OvUklHDQoArj9DESHvoHv8",
+  "authDomain": "fintrack-mini-pwa-296545-f7949.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "94520355691"
+};
