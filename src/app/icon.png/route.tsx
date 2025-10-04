541591c64f2a7af5063488190b4c39237f755503
david wu <davidwuguantong@gmail.com>
2025-10-04 03:52:06 +0000

    can you create a black icon with a green bolded dollar sign as my icon?

--- src/app/icon.png/route.tsx
+++ src/app/icon.png/route.tsx
@@ -19,33 +19,19 @@ export default function Icon() {
       // ImageResponse JSX element
       <div
         style={{
-          fontSize: 100,
-          background: '#1a1a1a', // A dark background matching the theme
+          fontSize: 128,
+          background: '#000000', // Black background
           width: '100%',
           height: '100%',
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
           color: '#4ade80', // A bright green color for the icon
-          borderRadius: '24px'
+          borderRadius: '24px',
+          fontWeight: 'bold',
         }}
       >
-        <svg 
-          xmlns="http://www.w3.org/2000/svg" 
-          width="128" 
-          height="128" 
-          viewBox="0 0 24 24" 
-          fill="none" 
-          stroke="currentColor" 
-          strokeWidth="2" 
-          strokeLinecap="round" 
-          strokeLinejoin="round"
-        >
-          <path d="M3 3v18h18"/>
-          <path d="M18.7 8a6 6 0 0 0-6 0"/>
-          <path d="M12.7 14a6 6 0 0 0-6 0"/>
-          <path d="M6.7 20a6 6 0 0 0-6 0"/>
-        </svg>
+        $
       </div>
     ),
     // ImageResponse options
